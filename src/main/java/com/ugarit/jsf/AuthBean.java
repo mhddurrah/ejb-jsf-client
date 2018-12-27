4df3f06672f4e730321a89ab8a91def7a779f0b4
Muhammed Durrah <mhd.durrah@gmail.com>
2018-12-27 23:39:42 +0300

    publish to github

---
+++ src/main/java/com/ugarit/jsf/AuthBean.java
@@ -0,0 +1,50 @@
+package com.ugarit.jsf;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named("auth")
+@SessionScoped
+public class AuthBean implements Serializable {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void authenticate() {
+        if ("admin".equals(username) && "admin".equals(password)) {
+            try {
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "message", null);
+                FacesContext.getCurrentInstance().addMessage(null, message);
+                FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+            } catch (Exception e) {
+                e.printStackTrace();
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "error", null);
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        } else {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", null);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+}
---
+++ src/main/java/com/ugarit/jsf/VehicleBean.java
@@ -0,0 +1,30 @@
+package com.ugarit.jsf;
+
+import com.ugarit.interfaces.VehicleRemote;
+import com.ugarit.persistence.entities.Vehicle;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named("vehicle")
+@RequestScoped
+public class VehicleBean implements Serializable {
+
+    @EJB
+    VehicleRemote vehicleRemote;
+
+    private List<Vehicle> vehicles;
+
+    @PostConstruct
+    public void postConstruct() {
+        vehicles = vehicleRemote.getVehicles();
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+}
